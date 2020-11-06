c0f6826cf57240fd8e3b9ae2ac05d17b72b5660d
Andres Espejo <jeaespejogo@unal.edu.co>
2020-11-06 12:24:05 -0500

    changes in the app color

--- components/Navbar/ShoppingCartIcon.tsx
+++ components/Navbar/ShoppingCartIcon.tsx
@@ -1,47 +1,48 @@
-import React from 'react'
-import { Basket } from '@components/SVGIcons'
+import React from 'react';
+import { Basket } from '@components/SVGIcons';
 
 type ShoppingCartIconProps = {
-  cartCount: number
-  name: string
-}
+    cartCount: number;
+    name: string;
+};
 
 const ShoppingCartIcon = ({ cartCount, name }: ShoppingCartIconProps) => {
-  const showCartCount = () => {
-    if (!cartCount) {
-      return `(0)`
-    }
-    if (cartCount > 9) {
-      return (
-        <span>
-          9<sup>+</sup>
-        </span>
-      )
-    }
-    return `(${cartCount})`
-  }
+    const showCartCount = () => {
+        if (!cartCount) {
+            return `(0)`;
+        }
+        if (cartCount > 9) {
+            return (
+                <span>
+                    9<sup>+</sup>
+                </span>
+            );
+        }
+        return `(${cartCount})`;
+    };
 
-  return (
-    <div className="container">
-      <Basket />
-      <div className="text">
-        {` ${name} `}
-        {showCartCount()}
-      </div>
-      <style jsx>{`
-        .container {
-          display: flex;
-          align-items: center;
-        }
-        .text {
-          margin-left: 0.5rem;
-        }
-        .text span {
-          font-size: smaller;
-        }
-      `}</style>
-    </div>
-  )
-}
+    return (
+        <div className="container">
+            <Basket />
+            <div className="text">
+                {` ${name} `}
+                {showCartCount()}
+            </div>
+            <style jsx>{`
+                .container {
+                    display: flex;
+                    align-items: center;
+                    background-color: aquamarine;
+                }
+                .text {
+                    margin-left: 0.5rem;
+                }
+                .text span {
+                    font-size: smaller;
+                }
+            `}</style>
+        </div>
+    );
+};
 
-export default ShoppingCartIcon
+export default ShoppingCartIcon;
